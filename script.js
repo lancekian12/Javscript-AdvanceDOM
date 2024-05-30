30616e0152c7e3fe515636ec5c44618364462f69
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-05-31 06:20:24 +0800

    added slider component javascript

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ const sectionObserver = new IntersectionObserver(
 );
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // Lazy loading images
@@ -245,6 +245,58 @@ const imgObserver = new IntersectionObserver(loadImg, {
   rootMargin: '-200px',
 });
 imgTargets.forEach(img => imgObserver.observe(img));
+
+/// Slider
+const slides = document.querySelectorAll('.slide');
+const btnleft = document.querySelector(
+  '.slider__btn--left'
+);
+const btnRight = document.querySelector(
+  '.slider__btn--right'
+);
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+// 0%, %, 200%, 300%
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) =>
+      (s.style.transform = `translateX(${
+        100 * (i - slide)
+      }%)`)
+  );
+};
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnleft.addEventListener('click', prevSlide);
+// curSlide = 1; -100%, 0%, 100%, 300%
+
 ///////////////////////
 ///////////////////////
 ///////////////////////
