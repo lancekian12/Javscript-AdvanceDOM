01edc74f054d96106df229050bbe49b4782409ae
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-05-31 01:14:17 +0800

    Applied event delegation into the project

--- script.js
+++ script.js
@@ -104,6 +104,39 @@ document
     }
   });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector(
+  '.operations__tab-container'
+);
+const tabsContent = document.querySelectorAll(
+  '.operations__content'
+);
+
+tabsContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t =>
+    t.classList.remove('operations__tab--active')
+  );
+  tabsContent.forEach(c =>
+    c.classList.remove('operations__content--active')
+  );
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(
+      `.operations__content--${clicked.dataset.tab}`
+    )
+    .classList.add('operations__content--active');
+});
 ///////////////////////
 ///////////////////////
 ///////////////////////
@@ -258,31 +291,31 @@ message.innerHTML =
 //     console.log('NAV', e.target, e.currentTarget);
 //   });
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-// Going downwards: child
-console.log(h1.querySelectorAll('.highlight'));
-console.log(h1.childNodes);
-console.log(h1.children);
-h1.firstElementChild.style.color = 'white';
-h1.lastElementChild.style.color = 'orangered';
+// // Going downwards: child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
 
-// Going upwards: parents
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// // Going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest('.header').style.background =
-  'var(--gradient-secondary)';
-h1.closest('.header').style.background =
-  'var(--gradient-secondary)';
+// h1.closest('.header').style.background =
+//   'var(--gradient-secondary)';
+// h1.closest('.header').style.background =
+//   'var(--gradient-secondary)';
 
-// Going sideways: siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
+// // Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
 
-console.log(h1.parentElement.children);
-[...h1.parentElement.children].forEach(function (el) {
-  if (el !== h1) el.style.transform = 'scale(0.5)';
-});
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = 'scale(0.5)';
+// });
