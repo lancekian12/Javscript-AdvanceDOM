11c63ae641b132aba0cc45d6e0bf52a7171b9c26
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-05-31 04:49:54 +0800

    Applied Intersection Observer and how to maximize performance of images

--- script.js
+++ script.js
@@ -184,11 +184,9 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 const header = document.querySelector('.header');
 const navHeigt = nav.getBoundingClientRect().height;
-console.log(navHeigt);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -199,6 +197,54 @@ const headObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `${navHeigt}px`,
 });
 headObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(
+  revealSection,
+  {
+    root: null,
+    threshold: 0.15,
+  }
+);
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Lazy loading images
+const imgTargets =
+  document.querySelectorAll('img[data-src');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 ///////////////////////
 ///////////////////////
 ///////////////////////
